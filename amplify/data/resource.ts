895f4d18a5b86edeb2a7e6c678fd3c57183a2fc6
brappai14 <35585878+brappai14@users.noreply.github.com>
2024-11-21 23:50:19 -0500

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,9 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  cust: a
     .model({
-      content: a.string(),
+      address: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
